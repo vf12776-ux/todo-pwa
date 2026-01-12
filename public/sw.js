28454a6523264a83a69c883a42fbb133afa9f0ac
none <https://github.com/vf12776-ux>
2026-01-12 03:24:39 +0300

    Исправлены пути в manifest.json и sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,16 +1,14 @@
-// Более продвинутый Service Worker
 const CACHE_NAME = 'todo-pwa-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/src/style.css',
-  '/src/app.js',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  '.',
+  'index.html',
+  'manifest.json',
+  'src/style.css',
+  'src/app.js',
+  'public/icons/icon-192.png',
+  'public/icons/icon-512.png'
 ];
 
-// Установка
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -18,42 +16,9 @@ self.addEventListener('install', event => {
   );
 });
 
-// Активация (очистка старых кешей)
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
-});
-
-// Запросы (сначала кеш, потом сеть)
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response; // Из кеша
-        }
-        return fetch(event.request) // Из сети
-          .then(response => {
-            // Кешируем новые ресурсы
-            return caches.open(CACHE_NAME)
-              .then(cache => {
-                cache.put(event.request, response.clone());
-                return response;
-              });
-          })
-          .catch(() => {
-            // Оффлайн: можно вернуть заглушку
-            return caches.match('/index.html');
-          });
-      })
+      .then(response => response || fetch(event.request))
   );
 });
